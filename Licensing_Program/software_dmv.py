3b92d3a5e470f22f739b94016d4f809692968e30
tjesser <tjesser@ucdavis.edu>
2015-11-30 17:42:57 -0800

    Change arg variable name.

--- Licensing_Program/software_dmv.py
+++ Licensing_Program/software_dmv.py
@@ -32,7 +32,7 @@ def create_main_parser():
     )
     subparsers = parser.add_subparsers(
         title="Commands",
-        dest="mode",
+        dest="command",
     )
 
     parser_check = subparsers.add_parser(
