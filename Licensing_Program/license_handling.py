341cd17f12c5c505ed45fe5ed504c101f5b8703b
tjesser <tjesser@ucdavis.edu>
2016-07-25 16:22:27 -0700

    Move license file handling to a separate file.

---
+++ Licensing_Program/license_handling.py
@@ -0,0 +1,54 @@
+"""This file provides functionality for using the license repository included
+ in the software_dmv project.
+"""
+
+import functools
+import os
+import re
+
+
+LICENSE_DIR = "Licenses"
+
+DEFAULT_LICENSE = "generic"
+
+LICENSE_EXT = "txt"
+LICENSE_HEADER_SUFFIX = "_header"
+
+
+@functools.lru_cache()
+def get_license_list():
+    return sorted({
+        os.path.basename(path).rstrip(LICENSE_HEADER_SUFFIX)
+        for dirpath, dirnames, filenames in os.walk(LICENSE_DIR)
+        for filename in filenames
+        for path, ext in [os.path.splitext(filename)]
+    })
+
+
+def get_license_info(license_name):
+    license_txtfile = open(
+        os.path.join(LICENSE_DIR, license_name + LICENSE_EXT),
+        "rt",
+    )
+    license_json = json.load(
+        open(
+            os.path.join(_license_dir, license_name + ".json"),
+            "rt",
+        )
+    )
+
+    try:
+        header_txtfile = open(
+            os.path.join(_license_dir, license_name + "_header.txt"),
+            "rt",
+        )
+    except FileNotFoundError:
+        header_txtfile = open(
+            os.path.join(_license_dir, _generic_headerfile),
+            "rt",
+        )
+
+    license_json["fullText"] = str(license_txtfile)
+    license_json["headerText"] = str(header_txtfile)
+
+    return license_json
