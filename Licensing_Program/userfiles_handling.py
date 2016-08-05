1a0a0ec3ff8b694d50ac7d7e82dee90437e1ad26
tjesser <tjesser@ucdavis.edu>
2016-08-05 13:43:08 -0700

    Rename check_file() to file_has_correct_header(). Add additional verbose information. Refactoring.

--- Licensing_Program/userfiles_handling.py
+++ Licensing_Program/userfiles_handling.py
@@ -20,40 +20,39 @@ def _find_header_start_line(path):
     return None
 
 
-def check_file(path, args, config):
+def file_has_correct_header(path, args, config):
     license_info = license_handling.get_license_info(config)
 
-    with open(path, "rt") as file:
-        # The copyright header should be in the first 20 lines
-        fileslice = itertools.islice(file, 20)
-        for linenum, line in enumerate(fileslice):
-            if "Copyright".casefold() in line.casefold():
-                break
-        else:
-            if args.info_level == "verbose":
+    linenum = _find_header_start_line(path)
+
+    if linenum is not None:
+        with open(path, "rt") as source_file:
+            source_file = itertools.islice(source_file, linenum, None)
+
+            header = license_handling.format_header(
+                license_info["Header"], path, config)
+            header_lines = (x + "\n" for x in header.splitlines())
+
+            nonmatching_lines = [
+                (header_line, file_line)
+                for header_line, file_line
+                in zip(header_lines, source_file)
+                if header_line != file_line
+            ]
+
+            if args.info_level == "verbose" and nonmatching_lines:
                 print(
-                    "File {} does not appear to have a license header.".format(
-                        file.name)
+                    ("In file {}: there are {} lines that do not match the"
+                     " expected license header.").format(file.name,
+                                                         len(nonmatching_lines))
                 )
 
-            return False
+            return not bool(nonmatching_lines)
 
-        header = format_header(license_info["Header"], path, config)
-        header_lines = (x + "\n" for x in header.splitlines())
-
-        # pylint: disable=undefined-loop-variable
-        nonmatching_lines = [
-            (header_line, file_line)
-            for header_line, file_line
-            in zip(header_lines, itertools.chain([line], file))
-            if header_line != file_line
-        ]
-
-        if args.info_level == "verbose" and nonmatching_lines:
+    else:
+        if args.info_level == "verbose":
             print(
-                ("In file {}: there are {} lines that do not match the"
-                 " expected license header.").format(file.name,
-                                                     len(nonmatching_lines))
+                "File {} does not appear to have a license header.".format(path)
             )
 
-        return not bool(nonmatching_lines)
+        return False
