a40f47e53352eb1cbaed6103b94f9b81a4990d04
tjesser <tjesser@ucdavis.edu>
2016-08-22 12:57:35 -0700

    Change get_license_list() to use licenses submodule. Add docstring.

--- Licensing_Program/license_handling.py
+++ Licensing_Program/license_handling.py
@@ -17,12 +17,9 @@ LICENSE_HEADER_SUFFIX = "_header"
 
 
 def get_license_list():
-    return sorted({
-        os.path.basename(path).rstrip(LICENSE_HEADER_SUFFIX)
-        for dirpath, dirnames, filenames in os.walk(LICENSE_DIR)
-        for filename in filenames
-        for path, ext in [os.path.splitext(filename)]
-    })
+"""Return a sorted list of supported license names.
+"""
+    return sorted(licenses.license_dict.keys())
 
 
 def get_license_files(license_name):
