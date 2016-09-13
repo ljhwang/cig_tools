6222b2715257d2a71a5a74be7f9eb6decda56d1d
tjesser <tjesser@ucdavis.edu>
2016-09-13 16:47:06 -0700

    Start moving parser creation into separate submodule.

---
+++ Licensing_Program/cli_parser/main_parser.py
@@ -0,0 +1,136 @@
+"""Create main parser for software_dmv program.
+"""
+
+import argparse
+
+
+def create_main_parser():
+    """Create CLI parser for this program.
+    """
+    parser = argparse.ArgumentParser(
+        description="Where you go to get your software license.",
+    )
+    subparsers = parser.add_subparsers(
+        title="Commands",
+        dest="command",
+    )
+    subparsers.required = True
+
+    parser_check = subparsers.add_parser(
+        "check",
+        help="Check all project files for licensing information",
+        description="Check all project files for licensing information.",
+    )
+    parser_check.add_argument(
+        "--no-ignore",
+        action="store_true",
+        help="Also check files that are set to be ignored",
+    )
+    parser_check.add_argument(
+        "-a",
+        "--add-missing",
+        action="store_true",
+        help="Add project license to files that fail the check",
+    )
+    parser_check.add_argument(
+        "-f",
+        "--files",
+        metavar="FILE",
+        nargs="+",
+        help="Check these files only",
+    )
+
+    verbosity_group = parser_check.add_mutually_exclusive_group()
+
+    verbosity_group.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        const="verbose",
+        dest="info_level",
+        default="",
+        help="Output more information about executed command",
+    )
+    verbosity_group.add_argument(
+        "-q",
+        "--quiet",
+        action="store_const",
+        const="quiet",
+        dest="info_level",
+        default="",
+        help="Silence all non-error output",
+    )
+
+    parser_choose = subparsers.add_parser(
+        "choose",
+        help="Choose a license to insert into your project",
+        description="Choose a license to insert into your project.",
+    )
+
+    verbosity_group = parser_choose.add_mutually_exclusive_group()
+
+    verbosity_group.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        const="verbose",
+        dest="info_level",
+        default="",
+        help="Output more information about executed command",
+    )
+    verbosity_group.add_argument(
+        "-q",
+        "--quiet",
+        action="store_const",
+        const="quiet",
+        dest="info_level",
+        default="",
+        help="Silence all non-error output",
+    )
+
+    parser_choose.add_argument(
+        "--no-apply",
+        action="store_false",
+        dest="apply_choice",
+        help="Write/update config file, but do not apply license to project",
+    )
+
+    parser_choose.add_argument(
+        "-p",
+        "--parameter",
+        action="append",
+        dest="parameters",
+        help=("Set a license parameter. Input is in key:value form and"
+              " multiple parameters can be set by using multiple flags or by"
+              " concatenating key-value pairs with commas. e.g. '--parameter="
+              "project:foo,author:bar'."),
+    )
+    parser_choose.add_argument(
+        "license",
+        metavar="LICENSE",
+        help=("License to be created for your project. Use the 'list' command"
+              " to see all supported licenses."),
+    )
+
+    parser_list = subparsers.add_parser(
+        "list",
+        help="List supported licenses",
+        description="List supported licenses.",
+    )
+
+    parser_settings = subparsers.add_parser(
+        "settings",
+        help="Show current license settings for your project",
+        description="Show current license settings for your project.",
+    )
+    parser_settings.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        const="verbose",
+        dest="info_level",
+        default="",
+        help="Show all settings and their documentation",
+    )
+
+    return parser
