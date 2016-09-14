0a2d35e060400596d991fff77fd684646490232f
tjesser <tjesser@ucdavis.edu>
2016-09-14 16:20:30 -0700

    Start 'config' command subparser.

---
+++ Licensing_Program/cli_parser/config_parser.py
@@ -0,0 +1,14 @@
+"""Submodule for creating parser command 'config'.
+"""
+
+def add_command(subparser):
+    """Add 'config' command to a subparser.
+    """
+
+    parser_config = subparser.add_parser(
+        "config",
+        help="create or validate a configuration file for a software project",
+        description=("Create or validate a configuration file for a software"
+                     " project."),
+    )
+# TODO: figure out config features/options
