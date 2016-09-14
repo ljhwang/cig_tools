e222851b88c2f1a99407cfe4a3b2a46b1b24b21d
tjesser <tjesser@ucdavis.edu>
2016-09-14 16:21:35 -0700

    Remove unnecessary local variables. Change help text formatting.

--- Licensing_Program/cli_parser/license_parser.py
+++ Licensing_Program/cli_parser/license_parser.py
@@ -5,17 +5,11 @@ def add_command(subparser):
     """Add 'license' command to a subparser.
     """
 
-    help_str = "list supported licenses or describe a given license"
-
-    description_str = (
-        "List supported licenses or, if a license name is provided, describe"
-        " the specified license."
-    )
-
     parser_license = subparser.add_parser(
         "license",
-        help=help_str,
-        description=description_str,
+        help="list supported licenses or describe a given license",
+        description=("List supported licenses or, if a license name is"
+                     " provided, describe the specified license."),
     )
     parser_license.add_argument(
         "license",
