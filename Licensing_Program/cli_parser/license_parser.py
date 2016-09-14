b6f5d3ff4c972c279ab7f04e0edd11b3139366f5
tjesser <tjesser@ucdavis.edu>
2016-09-14 15:23:18 -0700

    Make the description text more informative for the 'license' command.

--- Licensing_Program/cli_parser/license_parser.py
+++ Licensing_Program/cli_parser/license_parser.py
@@ -5,15 +5,17 @@ def add_command(subparser):
     """Add 'license' command to a subparser.
     """
 
-    help_str = (
-        "list supported licenses or, if a license name is provided, describe a"
-        " specified license"
+    help_str = "list supported licenses or describe a given license"
+
+    description_str = (
+        "List supported licenses or, if a license name is provided, describe"
+        " the specified license."
     )
 
     parser_license = subparser.add_parser(
         "license",
         help=help_str,
-        description=help_str + ".",
+        description=description_str,
     )
     parser_license.add_argument(
         "license",
