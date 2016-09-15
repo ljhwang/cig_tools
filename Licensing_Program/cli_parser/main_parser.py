c84835f5c892b35914af4a589dcfc167f32b4455
tjesser <tjesser@ucdavis.edu>
2016-09-15 11:55:08 -0700

    Use config_parser and license_parser command submodules.

--- Licensing_Program/cli_parser/main_parser.py
+++ Licensing_Program/cli_parser/main_parser.py
@@ -4,6 +4,9 @@
 import argparse
 
 import cli_parser.check_parser
+import cli_parser.config_parser
+import cli_parser.license_parser
+
 
 def create_main_parser():
     """Creates main parser for the commandline interface.
@@ -34,63 +37,16 @@ def create_main_parser():
         help="silence all non-error output",
     )
 
-    subparsers = parser.add_subparsers(
-        title="Commands",
+    command_parser = parser.add_subparsers(
+        title="commands",
         dest="command",
     )
-    subparsers.required = True
+    command_parser.required = True
 
-    subparsers = cli_parser.check_parser.add_command(subparsers)
+    command_parser = cli_parser.check_parser.add_command(command_parser)
 
-    parser_choose = subparsers.add_parser(
-        "choose",
-        help="Choose a license to insert into your project",
-        description="Choose a license to insert into your project.",
-    )
+    command_parser = cli_parser.config_parser.add_command(command_parser)
 
-    parser_choose.add_argument(
-        "--no-apply",
-        action="store_false",
-        dest="apply_choice",
-        help="Write/update config file, but do not apply license to project",
-    )
-
-    parser_choose.add_argument(
-        "-p",
-        "--parameter",
-        action="append",
-        dest="parameters",
-        help=("Set a license parameter. Input is in key:value form and"
-              " multiple parameters can be set by using multiple flags or by"
-              " concatenating key-value pairs with commas. e.g. '--parameter="
-              "project:foo,author:bar'."),
-    )
-    parser_choose.add_argument(
-        "license",
-        metavar="LICENSE",
-        help=("License to be created for your project. Use the 'list' command"
-              " to see all supported licenses."),
-    )
-
-    parser_list = subparsers.add_parser(
-        "list",
-        help="List supported licenses",
-        description="List supported licenses.",
-    )
-
-    parser_settings = subparsers.add_parser(
-        "settings",
-        help="Show current license settings for your project",
-        description="Show current license settings for your project.",
-    )
-    parser_settings.add_argument(
-        "-v",
-        "--verbose",
-        action="store_const",
-        const="verbose",
-        dest="info_level",
-        default="",
-        help="Show all settings and their documentation",
-    )
+    command_parser = cli_parser.license_parser.add_command(command_parser)
 
     return parser
