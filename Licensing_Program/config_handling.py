2618b0365c433d8c5a1b23e53eefd22ceb790035
tjesser <tjesser@ucdavis.edu>
2016-09-20 11:18:14 -0700

    Reword docstring for param_ignoredfiles_to_regex().

--- Licensing_Program/config_handling.py
+++ Licensing_Program/config_handling.py
@@ -161,7 +161,8 @@ def antpattern_to_regex(pattern):
 def param_ignoredfiles_to_regex(ignore_string):
     """Converts a configuration pattern for 'IgnoredFiles' to a regex string.
     A pattern can have a 're:' prefix to denote it is already a regex or a
-    'ant:' prefix to explicitly show it is an ant pattern.
+    'ant:' prefix to explicitly show it is an ant pattern. Without a prefix,
+    an ant pattern is assumed.
     """
     if ignore_string.startswith("re:"):
         return ignore_string.lstrip("re:")
