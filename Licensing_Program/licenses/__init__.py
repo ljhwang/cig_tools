fe7a75b8feb90906a2bf4873deb2003640880be4
tjesser <tjesser@ucdavis.edu>
2016-10-13 14:39:14 -0700

    Add mit license to licenses submodule.

--- Licensing_Program/licenses/__init__.py
+++ Licensing_Program/licenses/__init__.py
@@ -5,9 +5,11 @@ in the first line of their file header.
 
 import licenses.gpl_2
 import licenses.gpl_3
+import licenses.mit
 
 
 license_dict = {
     "gpl-2" : licenses.gpl_2,
     "gpl-3" : licenses.gpl_3,
+    "mit" : licenses.mit,
 }
