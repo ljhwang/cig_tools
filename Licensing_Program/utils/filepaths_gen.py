3ab1bfa7eb73df2f7c48106bc8c8b271a51ce269
tjesser <tjesser@ucdavis.edu>
2016-11-16 15:30:04 -0800

    Docstring changes in filepaths_gen.

--- Licensing_Program/utils/filepaths_gen.py
+++ Licensing_Program/utils/filepaths_gen.py
@@ -22,7 +22,7 @@ def _is_visible_path(path: pathlib.PurePath) -> bool:
 
 def _os_walk_filter(filter_func, top_dir):
     """Generator for filtered `os.walk` concrete paths. `filter_func` must
-    accept a `pathlib.Path` relative to `top_dir`.
+    accept a `pathlib.PurePath` relative to `top_dir`.
     """
     for cwd, dirs, files in os.walk(top_dir):
         for dirname in dirs:
@@ -37,9 +37,9 @@ def _os_walk_filter(filter_func, top_dir):
 
 
 def filepaths_gen(top_dir, include_hidden=False):
-    """Generator for concrete filepaths in `top_dir` and sub directores. If
-    `include_hidden` is false, the generator will not include hidden files or
-    files in hidden directories. All paths will be relative to `top_dir`.
+    """Generator of `pathlib.Path`s for files in `top_dir` and sub directores.
+    If `include_hidden` is `False`, the generator will not include hidden files
+    or files in hidden directories. All `Path`s will be relative to `top_dir`.
     """
     if not include_hidden:
         return _os_walk_filter(_is_visible_path, top_dir)
