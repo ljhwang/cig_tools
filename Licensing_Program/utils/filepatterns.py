6880550924727e050557f15b61ba602a63ba775e
tjesser <tjesser@ucdavis.edu>
2016-11-29 10:58:13 -0800

    Create horrible bracket parsing.

--- Licensing_Program/utils/filepatterns.py
+++ Licensing_Program/utils/filepatterns.py
@@ -2,26 +2,86 @@
 import re
 
 
+def _bracket_tagged_partitions(string):
+    """Partitions `string` into pairs of tags and non-bracketed or bracketed
+    strings. Tags are either `None` or `'brackets'`. Raises an error if
+    brackets are not closed.
+    """
+    ret = []
+    bracket_begin = r"^|[^\\]\["
+    bracket_end = r"[^\\]\]"
+
+    def _bracket_parse(string):
+        """Partitions on first bracket pair.
+        """
+        begin_match = re.search(bracket_begin, string)
+
+        if begin_match:
+            end_match = re.search(bracket_end, string[begin_match.end():])
+            if end_match:
+                prefix = string[:begin_match.end() - 1]
+                brackets = string[begin.match.end() - 1:end_match.end()]
+                suffix = string[end_match.end():]
+
+                return (
+                    [(None, prefix)] if prefix else []
+                    + [('brackets', brackets)]
+                    + [(None, suffix)] if suffix else []
+                )
+
+            else:
+                raise ValueError("'string' did not contain closing bracket.")
+
+        else:
+            return [(None, string)]
+
+    parsed = _bracket_parse(string)
+
+    while len(parsed) > 1 and parsed[-1][0] is None:
+        tag, string = parsed.pop(-1)
+        ret += parsed
+        parsed = _bracket_parse(string)
+
+    return ret + parsed
+
+
 def filepattern_to_regex(pattern):
     """Convert config filepattern to filepath regex. Assumes POSIX path
     separator (/).
     """
-    # escape regex symbols
-    pattern = re.escape(pattern)
-
-    # double star (**) recurses through directories
-
-
-    # star (*) in a pattern without path separators is a basename wildcard
+    # escaped symbols: \ . ? $ ^ ( ) { } + |
+    # used symbols: [ ] * **
+    # path symbol: /
 
 
-    # star (*) in a pattern with path separators is a directory or file name
-    # wildcard
+
+    # double stars connected to path separators (**/) (/**/) (/**) recurse
+    # through directories
 
 
-    # patterns that end with a path separator match everything under that
-    # directory
+    # a pattern without path separators is a basename pattern
+    if "/" not in pattern:
+        pattern = re.sub(
+            r"^",
+            r"^|(?:.*?/)",
+            pattern,
+        )
 
+    # star (*) is a path part wildcard (unless in brackets)
+    pattern = re.sub(
+        r"*",
+        r"[^/]*?",
+        pattern,
+    )
+
+
+    # patterns that end with a path separator recurse through all
+    # subdirectories
+    pattern = re.sub(
+        r"/$",
+        r"/.*",
+        pattern,
+    )
 
     # brackets ([]) are regex brackets
 
