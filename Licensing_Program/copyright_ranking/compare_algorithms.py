c583d7c70c758b5b75b1717d0a0cc1deac56a951
tjesser <tjesser@ucdavis.edu>
2017-01-12 12:32:05 -0800

    Move sameness distribution histograms into separate file.

--- Licensing_Program/copyright_ranking/compare_algorithms.py
+++ Licensing_Program/copyright_ranking/compare_algorithms.py
@@ -11,6 +11,7 @@ import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 
 from bar_graph_manual_licenses import bar_graph_manual_licenses
+from hist_sameness_distributions import hist_sameness_distributions
 
 CONFIG = {
     "Specfem3dDB" : "specfem3d_license_info.db",
@@ -64,142 +65,9 @@ if __name__ == "__main__":
         uri=True,
     ) as conn:
         cursor = conn.cursor()
-
         bar_graph_manual_licenses(cursor, CONFIG["ColorList"])
 
-        # histograms showing rank distribution for correctly and incorrectly
-        # ranked licenses (matching or not matching manual license
-        # determination)
-        rank_dist_fig = plt.figure()
-        rank_dist_fig.suptitle(
-            "Sameness Distribution"
-        )
-        rank_dist_license_fig = plt.figure()
-        rank_dist_license_fig.suptitle(
-            "Sameness Distribution per License"
-        )
-
-        rank_dist_fig.add_subplot()
-        rank_dist_ax = rank_dist_fig.gca()
-
-        cursor = cursor.execute("""
-            SELECT
-              calculated_license_rank.license = project_files.manual_license,
-              ranking_algorithms.name,
-              licenses.name,
-              calculated_license_rank.ranking
-            FROM
-                  calculated_license_rank
-              JOIN
-                  project_files
-                ON
-                  calculated_license_rank.file = project_files.id
-              JOIN
-                  ranking_algorithms
-                ON
-                  calculated_license_rank.algorithm = ranking_algorithms.id
-              JOIN
-                  licenses
-                ON
-                  project_files.manual_license = licenses.id
-            GROUP BY
-                calculated_license_rank.file,
-                calculated_license_rank.algorithm
-              HAVING
-                MAX(calculated_license_rank.ranking)
-        """)
-
-        data = defaultdict(list)
-        data_per_license = defaultdict(lambda: defaultdict(list))
-
-        for match, algorithm, manual_license, ranking in cursor:
-            data[(bool(match), algorithm)].append(ranking)
-
-            data_per_license[manual_license][
-                (bool(match), algorithm)
-            ].append(ranking)
-
-        data_keys, data_values = zip(*sorted(data.items()))
-
-        rank_dist_ax.hist(
-            data_values,
-            range=(0,1),
-            bins=50,
-            color=list(islice(CONFIG["ColorList"], len(data))),
-            edgecolor="None",
-            label=[
-                "{} - {} {}".format(
-                    algorithm,
-                    len(data[(match, algorithm)]),
-                    "correctly ranked" if match else "incorrectly ranked",
-                )
-                for match, algorithm in data_keys
-            ],
-        )
-
-        rank_dist_ax.set_xticklabels([
-            "{:.0%}".format(tick)
-            for tick in rank_dist_ax.get_xticks()
-        ])
-
-        rank_dist_ax.set_xlabel("Algorithm Result (Sameness)")
-        rank_dist_ax.legend(loc="best")
-        rank_dist_ax.grid(True)
-
-        data_per_license = dict(
-            filter(
-                lambda pair: 4 < sum(len(ranks) for ranks in pair[1].values()),
-                data_per_license.items(),
-            )
-        )
-
-        for index, (license, license_dict) in enumerate(
-            sorted(
-                data_per_license.items(),
-                key=lambda pair: sum(len(ranks) for ranks in pair[1].values()),
-                reverse=True,
-            ),
-            1,
-        ):
-            license_dict_keys, license_dict_values = zip(
-                *sorted(license_dict.items(), reverse=True)
-            )
-
-            rank_dist_license_fig.add_subplot(
-                math.floor(len(data_per_license) ** (1 / 2)),
-                math.ceil(len(data_per_license) ** (1 / 2)),
-                index,
-            )
-            ax = rank_dist_license_fig.gca()
-
-            ax.hist(
-                license_dict_values,
-                range=(0,1),
-                bins=50,
-                label=[
-                    "{} - {} {}".format(
-                        algorithm,
-                        len(license_dict[(match, algorithm)]),
-                        "correctly ranked" if match else "incorrectly ranked",
-                    )
-                    for match, algorithm in license_dict_keys
-                ],
-                color=list(islice(
-                    CONFIG["ColorList"],
-                    len(license_dict) + len(license_dict) % 2,
-                ))[:len(license_dict)],
-                edgecolor="None",
-                width=1 / 100,
-                rwidth=1,
-            )
-
-            ax.set_xticklabels([
-                "{:.0%}".format(tick)
-                for tick in ax.get_xticks()
-            ])
-
-            ax.grid(True)
-            ax.legend(loc='best', fontsize="small")
-            ax.set_title("{}".format(license))
+        cursor = conn.cursor()
+        hist_sameness_distributions(cursor, CONFIG["ColorList"])
 
         plt.show()
