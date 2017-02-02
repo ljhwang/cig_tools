0f53ff66c6aff9650a8ee0b4ba923c20ec4378a2
tjesser <tjesser@ucdavis.edu>
2017-02-02 12:34:58 -0800

    Finish implementation of parser. Can now convert filepatterns to regex.

--- Licensing_Program/utils/filepatterns/ast_types.py
+++ Licensing_Program/utils/filepatterns/ast_types.py
@@ -1,54 +1,227 @@
 """Module that exports AST types for the filepatterns parser.
 """
 
+import enum
+
 import rply.token
 
 
-class UnitBox(rply.token.BaseBox):
+@enum.unique
+class PatternType(enum.Enum):
+    root = 1
+    part = 2
+
+@enum.unique
+class SlashType(enum.Enum):
+    trailing = 1
+    nontrail = 2
+
+
+### Tokens ### Tokens ### Tokens ### v
+
+class Token(rply.token.BaseBox):
+    def __init__(self):
+        pass
+
+    def regex(self, **kwargs):
+        raise NotImplementedError("Token regex call on {}".format(self))
+
+
+class SlashToken(Token):
+    def regex(self, **kwargs):
+        return "/"
+
+
+class WildPathToken(Token):
+    def regex(self, **kwargs):
+        return ".*"
+
+
+class AsteriskToken(Token):
+    def regex(self, **kwargs):
+        return "[^/]*"
+
+
+class NoAsteriskToken(Token):
     def __init__(self, token):
         self.token = token
 
-    def getstr(self):
-        return self.token.getstr()
+    def regex(self, **kwargs):
+        if isinstance(self.token, EscapedSymbolToken):
+            return self.token.regex(**kwargs)
+        else:
+            return self.token.getstr()
 
 
-class ListBox(rply.token.BaseBox):
-    def __init__(self, *tokens):
-        self.tokens = list(tokens)
+class EscapedSymbolToken(Token):
+    def __init__(self, tokens):
+        self.tokens = tokens
 
-    def __add__(self, listbox):
-        self.tokens += listbox.tokens
-        return self
-
-    def getstr(self):
+    def regex(self, **kwargs):
         return "".join(token.getstr() for token in self.tokens)
 
-
-class EscapedSymbol(UnitBox):
-    def getstr(self):
-        return "\\" + self.token.getstr()
+### Tokens ### Tokens ### Tokens ### ^
 
 
-class BracketSet(ListBox):
-    def getstr(self):
-        return "[{}]".format("".join(token.getstr() for token in self.tokens))
+### Patterns ### Patterns ### Patterns ### v
 
+class Pattern(rply.token.BaseBox):
+    pattern_type = None
 
-class NoAtrskPart(ListBox):
-    pass
+    def __init__(self, ast):
+        self.ast = ast
 
+    def regex(self, pattern_type=None, **kwargs):
+        if pattern_type is None:
+            ret = ""
+            if self.pattern_type == PatternType.root:
+                ret = ".*"
+            elif self.pattern_type == PatternType.part:
+                ret = ""
 
-class NonTrailAtrskPart(ListBox):
-    pass
-
-
-class TrailingAtrskPart(ListBox):
-    def getstr(self, root_pattern=False):
-        if root_pattern:
-            pass
+            return ret + self.ast.regex(
+                pattern_type=self.pattern_type, **kwargs
+            )
         else:
-            pass
+            return self.ast.regex(
+                pattern_type=pattern_type, **kwargs
+            )
 
 
-class PathPart(ListBox):
-    pass
+class PathPattern(Pattern):
+    pattern_type = PatternType.part
+
+
+class RootPattern(Pattern):
+    pattern_type = PatternType.root
+
+### Patterns ### Patterns ### Patterns ### ^
+
+
+### SlashPatterns ### SlashPatterns ### SlashPatterns ### v
+
+class SlashPattern(rply.token.BaseBox):
+    slash_type = None
+
+    def __init__(self, ast_seq):
+        if isinstance(ast_seq[0], SlashPattern):
+            self.ast_seq = ast_seq[0].ast_seq + [ast_seq[1]]
+        else:
+            self.ast_seq = ast_seq
+
+    def regex(self, **kwargs):
+        if self.slash_type == SlashType.trailing:
+            return "".join(
+                ast.regex(**kwargs)
+                for ast in self.ast_seq
+            ) + WildPathToken().regex()
+
+        elif self.slash_type == SlashType.nontrail:
+            return "".join(
+                ast.regex(**kwargs)
+                for ast in self.ast_seq
+            )
+
+
+class TrailingSlashPattern(SlashPattern):
+    slash_type = SlashType.trailing
+
+
+class NontrailSlashPattern(SlashPattern):
+    slash_type = SlashType.nontrail
+
+### SlashPatterns ### SlashPatterns ### SlashPatterns ### ^
+
+
+### PathPart ### PathPart ### PathPart ### v
+
+class PathPart(rply.token.BaseBox):
+    def __init__(self, ast):
+        self.ast = ast
+
+    def regex(self, **kwargs):
+        return self.ast.regex(**kwargs)
+
+### PathPart ### PathPart ### PathPart ### ^
+
+
+### AsteriskPart ### AsteriskPart ### AsteriskPart ### v
+
+class AsteriskPart(rply.token.BaseBox):
+    def __init__(self, ast_seq):
+        if isinstance(ast_seq[0], AsteriskPart):
+            self.ast_seq = ast_seq[0].ast_seq + [ast_seq[1]]
+        else:
+            self.ast_seq = ast_seq
+
+    def regex(self, **kwargs):
+        return "".join(
+            ast.regex(**kwargs)
+            for ast in self.ast_seq
+        )
+
+### AsteriskPart ### AsteriskPart ### AsteriskPart ### ^
+
+
+### NoAsteriskPart ### NoAsteriskPart ### NoAsteriskPart ### v
+
+class NoAsteriskPart(rply.token.BaseBox):
+    def __init__(self, ast_seq):
+        if isinstance(ast_seq[0], NoAsteriskPart):
+            self.ast_seq = ast_seq[0].ast_seq + [ast_seq[1]]
+        else:
+            self.ast_seq = ast_seq
+
+    def regex(self, **kwargs):
+        return "".join(
+            ast.regex(**kwargs)
+            for ast in self.ast_seq
+        )
+
+### NoAsteriskPart ### NoAsteriskPart ### NoAsteriskPart ### ^
+
+
+### NoAtrskBracketSet ### NoAtrskBracketSet ### NoAtrskBracketSet ### v
+
+class NoAtrskBracketSet(rply.token.BaseBox):
+    def __init__(self, ast):
+        self.ast = ast
+
+    def regex(self, **kwargs):
+        return self.ast.regex(**kwargs)
+
+### NoAtrskBracketSet ### NoAtrskBracketSet ### NoAtrskBracketSet ### ^
+
+
+### BracketSet ### BracketSet ### BracketSet ### v
+
+class BracketSet(rply.token.BaseBox):
+    def __init__(self, ast_seq):
+        if isinstance(ast_seq[0], BracketSet):
+            self.ast_seq = ast_seq[0].ast_seq + [ast_seq[1]]
+        else:
+            self.ast_seq = ast_seq
+
+    def regex(self, **kwargs):
+        return "[" + "".join(
+            ast.regex(**kwargs)
+            for ast in self.ast_seq
+        ) + "]"
+
+### BracketSet ### BracketSet ### BracketSet ### ^
+
+
+### BracketElem ### BracketElem ### BracketElem ### v
+
+class BracketElem(rply.token.BaseBox):
+    def __init__(self, ast):
+        self.ast = ast
+
+    def regex(self, **kwargs):
+        if isinstance(self.ast, EscapedSymbolToken):
+            return self.ast.regex(**kwargs)
+        else:
+            return self.ast.getstr()
+
+### BracketElem ### BracketElem ### BracketElem ### ^
+
